ef9407b3fa89897ada50b4e52d1d72c6ae6656ab
Noah Snelson <noah.snelson@protonmail.com>
2019-11-29 01:46:52 -0800

    Able to evaluate builtin values and default words

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,13 +1,18 @@
 module Eval where
 
 import Parser
+import Data.IORef
+import qualified Data.Map.Strict as M
 
-eval :: LispVal -> IO ()
-eval (String str) = print str
-eval (Integer int) = print int
-eval (Word name) = print name
-eval (Boolean bool) = print bool
-eval (List [Word "quote", val]) = print val
-eval (List (Word fun : val)) = undefined
-eval _ = undefined
+--type Runtime = IORef Environment -- for keeping track of all words
+type Environment = M.Map String ([LispVal] -> LispVal) -- map words to values
 
+eval :: Environment -> LispVal -> LispVal
+eval _ (List [Word "quote", val]) = val
+eval env (List (Word fun : args)) = apply env fun $ map (eval env) args
+eval _ val = val
+
+apply :: Environment -> String -> [LispVal] -> LispVal
+apply env fun args = case M.lookup fun env of
+                        Just f -> f args
+                        Nothing -> error "Word not found."
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,21 @@
 module Lib ( 
-parse
+repl
 ) where
 
 import ParserType
 import Parser
 import Lexer
 import Eval
+import qualified Data.Map.Strict as M
+
+defaultEnv :: Environment
+defaultEnv = M.fromList [("+", \[Integer x, Integer y] -> Integer (x + y)),
+                         ("-", \[Integer x, Integer y] -> Integer (x - y))]
+
+repl :: IO ()
+repl = do
+    input <- getLine
+    let ast = parse input
+    let out = eval defaultEnv ast
+    print out
+    repl
