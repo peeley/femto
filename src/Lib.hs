0748b2d5bd73db34185952baa061e707eded7d59
Noah Snelson <noah.snelson@protonmail.com>
2019-11-29 22:16:41 -0800

    Added list functions to default words

--- src/Lib.hs
+++ src/Lib.hs
@@ -15,8 +15,32 @@ defaultEnv = M.fromList [("+", \[Integer x, Integer y] -> Integer (x + y)),
                          ("*", \[Integer x, Integer y] -> Integer (x * y)),
                          ("/", \[Integer x, Integer y] -> Integer (x `div` y)),
                          ("^", \[Integer x, Integer y] -> Integer (x ^ y)),
-                         ("=", \[x, y] -> Boolean (x==y)),
-                         ("!=", \[x, y] -> Boolean (x/=y))]
+                         ("=", \[x, y] -> Boolean (x == y)),
+                         ("!=", \[x, y] -> Boolean (x /= y)),
+                         ("<", \[Integer x, Integer y] -> Boolean (x < y)),
+                         (">", \[Integer x, Integer y] -> Boolean (x > y)),
+                         ("<=", \[Integer x, Integer y] -> Boolean (x <= y)),
+                         (">=", \[Integer x, Integer y] -> Boolean (x >= y)),
+                         ("&&", \[Boolean x, Boolean y] -> Boolean (x && y)),
+                         ("||", \[Boolean x, Boolean y] -> Boolean (x || y)),
+                         ("car", car),
+                         ("cdr", cdr),
+                         ("cons", cons)]
+
+car :: [LispVal] -> LispVal
+car [List (x:xs)] = List [x]
+car [List []] = error "Empty list"
+car _ = error "Arg not valid list"
+
+cdr :: [LispVal] -> LispVal
+cdr [List (x:xs)] = List xs
+cdr [List []] = error "Empty list"
+cdr _ = error "Arg not valid list"
+
+cons :: [LispVal] -> LispVal
+cons [List x, List y] = List (x++y)
+cons [x, List y] = List (x:y)
+cons _ = error "Unable to combine args"
 
 repl :: IO ()
 repl = do
